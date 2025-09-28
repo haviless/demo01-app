ac0c00552f0881a7056cfa709ca1dbc63d2f94d0
SMARTLIFE PERU <haviless@hotmail.com>
2025-09-28 14:20:27 +0000

    version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Aplicacion ejemplo 01")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Documentacion de Streamlit [docs.streamlit.io](https://docs.streamlit.io/)."
 )
